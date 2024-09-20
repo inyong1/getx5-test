e4b4e377a61be1cd14a13b87737e54681b8ca6ab
Supriyadi <inyongmobile@gmail.com>
2024-09-20 16:06:23 +0700

    testing pop scope

--- lib/app/modules/halaman_tiga/controllers/halaman_tiga_controller.dart
+++ lib/app/modules/halaman_tiga/controllers/halaman_tiga_controller.dart
@@ -1,23 +1,7 @@
 import 'package:get/get.dart';
 
 class HalamanTigaController extends GetxController {
-  //TODO: Implement HalamanTigaController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  bool canPop = false;
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/halaman_tiga/views/halaman_tiga_view.dart
+++ lib/app/modules/halaman_tiga/views/halaman_tiga_view.dart
@@ -9,26 +9,59 @@ class HalamanTigaView extends GetView<HalamanTigaController> {
   const HalamanTigaView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HalamanTigaView'),
-        centerTitle: true,
-      ),
-      body:  Center(
-        child: Column( mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'HalamanTigaView is working',
-              style: TextStyle(fontSize: 20),
-            ),
-             ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(Routes.HALAMAN_EMPAT);
-                },
-                child: const Text('Go to halaman empat')),
-          ],
+    return PopScope(
+          canPop: controller.canPop,
+          onPopInvokedWithResult: (didPop, result) async {
+            if (didPop) {
+              return;
+            }
+            if (await _showConfirmationDialog(context)) {
+              Get.back();
+            }
+          },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('HalamanTigaView'), centerTitle: true),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'HalamanTigaView is working',
+                style: TextStyle(fontSize: 20),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(Routes.HALAMAN_EMPAT);
+                  },
+                  child: const Text('Go to halaman empat')),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _showConfirmationDialog(BuildContext context) async {
+    bool? confirm = await showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Confirmation"),
+          content: const Text("Are you sure want to leave me?"),
+          actions: [
+            MaterialButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("tidak jadi"),
+            ),
+            FilledButton(
+              onPressed: () => Navigator.pop(context, true),
+              child: const Text("Yes, of course"),
+            ),
+          ],
+        );
+      },
+    );
+    return confirm ?? false;
   }
 }
